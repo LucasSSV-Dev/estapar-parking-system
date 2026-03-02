99f8cad089d15d49e3ae0d3808e8a64e0db22d7c
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-02 16:00:58 -0300

    🎉 Commit inicial

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/EstaparParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Estapar.EstaparParkingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaparParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaparParkingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Estapar/EstaparParkingSystem/EstaparParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Estapar.EstaparParkingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstaparParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
