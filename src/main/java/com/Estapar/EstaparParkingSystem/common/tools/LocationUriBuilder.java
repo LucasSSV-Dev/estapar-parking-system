490f7cdcc9f3539eed735eb110208e33cbf7c92f
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-02 16:12:13 -0300

    ✨ feat: Implementa API e Controller para gerenciamento de eventos de estacionamento

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/common/tools/LocationUriBuilder.java
@@ -0,0 +1,19 @@
+package com.Estapar.EstaparParkingSystem.common.tools;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+@Component
+public class LocationUriBuilder {
+
+    public URI build(UUID id){
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
