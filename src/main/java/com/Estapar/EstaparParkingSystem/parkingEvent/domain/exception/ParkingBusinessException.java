4ef58ab0fb680edbdac69f1f7d29374715c74e48
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-02 16:13:15 -0300

    ✨ feat: Adiciona handler global de exceções e exceção de negócio para eventos de estacionamento

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingEvent/domain/exception/ParkingBusinessException.java
@@ -0,0 +1,7 @@
+package com.Estapar.EstaparParkingSystem.parkingEvent.domain.exception;
+
+public class ParkingBusinessException extends RuntimeException {
+    public ParkingBusinessException(String message) {
+        super(message);
+    }
+}
