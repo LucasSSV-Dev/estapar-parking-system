1933b82e4028ca8fc3ca0c3fb499c16287637cfe
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 17:40:54 -0300

    ✨ feat: Adiciona método `findAvailable` ao `GarageRepository` para listar garagens com vagas disponíveis

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/GarageRepository.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/GarageRepository.java
@@ -1,9 +1,20 @@
 package com.Estapar.EstaparParkingSystem.parkingSystem.domain.repository;
 
 import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.Garage;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GarageRepository extends JpaRepository<Garage, Long> {
+
+    @Query("""
+   select g from Garage g
+   where g.currentOccupancy < g.maxCapacity
+   order by g.currentOccupancy asc
+""")
+    List<Garage> findAvailable(PageRequest pageRequest);
 }
