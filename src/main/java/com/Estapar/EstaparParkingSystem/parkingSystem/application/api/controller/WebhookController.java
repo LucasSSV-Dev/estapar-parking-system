8ca6434af2ab40e1175c78230011c1de731e9f48
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 11:41:21 -0300

    ✨ feat: Implementa `WebhookController` para processar eventos recebidos

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/controller/WebhookController.java
@@ -0,0 +1,26 @@
+package com.Estapar.EstaparParkingSystem.parkingSystem.application.api.controller;
+
+import com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto.WebhookEventRequestDTO;
+import com.Estapar.EstaparParkingSystem.parkingSystem.application.service.WebhookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/webhook")
+public class WebhookController {
+
+    private final WebhookService webhookService;
+
+    @PostMapping
+    public ResponseEntity<Void> handleEvent(
+            @RequestBody WebhookEventRequestDTO request) {
+
+        webhookService.process(request);
+        return ResponseEntity.ok().build();
+    }
+}
