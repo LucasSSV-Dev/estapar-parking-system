d9fbb59ff6d1e82c379d163ab48ec472ccbf66d5
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 18:24:22 -0300

    ✨ feat: Adiciona tratamento de exceção para `BadRequestException` em `GlobalExceptionHandler`

--- src/main/java/com/Estapar/EstaparParkingSystem/common/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/common/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.Estapar.EstaparParkingSystem.common.Exception;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,16 +13,16 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(ParkingBusinessException.class)
-//    public ResponseEntity<Map<String, Object>> handleBusinessException(ParkingBusinessException exception) {
-//        Map<String, Object> body = new HashMap<>();
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("status", HttpStatus.BAD_REQUEST.value());
-//        body.put("error", "Business Error");
-//        body.put("message", exception.getMessage());
-//
-//        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-//    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String, Object>> handleBusinessException(BadRequestException exception) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Business Error");
+        body.put("message", exception.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGenericException(Exception exception) {
