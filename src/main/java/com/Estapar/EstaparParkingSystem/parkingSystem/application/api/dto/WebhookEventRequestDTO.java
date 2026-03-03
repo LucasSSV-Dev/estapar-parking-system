c41a4ea13664929a14ec77c5cfaabb7f87aa46f3
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 17:40:17 -0300

    ✨ feat: Adiciona anotações @JsonProperty ao `WebhookEventRequestDTO` para mapeamento de propriedades

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/dto/WebhookEventRequestDTO.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/dto/WebhookEventRequestDTO.java
@@ -1,18 +1,27 @@
 package com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto;
 
 import com.Estapar.EstaparParkingSystem.parkingSystem.domain.enums.EventType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record WebhookEventRequestDTO(
+        @JsonProperty("license_plate")
+        @NotNull
         String licensePlate,
+
+        @JsonProperty("event_type")
         @NotNull
         EventType eventType,
-        @NotNull
+
+        @JsonProperty("entry_time")
         LocalDateTime entryTime,
 
+        @JsonProperty("exit_time")
         LocalDateTime exitTime,
+
         Double lat,
+
         Double lng
 ) {}
