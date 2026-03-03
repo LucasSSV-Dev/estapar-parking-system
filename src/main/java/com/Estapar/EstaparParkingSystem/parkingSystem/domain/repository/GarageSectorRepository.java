f9f9e263267a880fe0e090d5c2f0aceaa25c8cc2
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 15:58:12 -0300

    🔥 refactor: Remove `GarageImportService`, `GarageSector`, and associated repository

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/GarageSectorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Estapar.EstaparParkingSystem.parkingSystem.domain.repository;
-
-import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.GarageSector;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GarageSectorRepository extends JpaRepository<GarageSector, String> {
-}
