79a2dc488560c6791c82b0ccb71b6d63b1c68b0c
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 17:40:48 -0300

    ✨ feat: Adiciona método `findByLatitudeAndLongitude` ao `ParkingSpotRepository`

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingSpotRepository.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingSpotRepository.java
@@ -4,6 +4,9 @@ import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
+    Optional<ParkingSpot> findByLatitudeAndLongitude(double latitude, double longitude);
 }
