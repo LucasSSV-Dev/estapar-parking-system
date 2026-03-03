ae0272796a58c17a43b0705fcc6405a6a030ce11
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 18:23:48 -0300

    ✨ feat: Adiciona método `findTopByLicensePlateAndExitTimeIsNullOrderByEntryTimeDesc` ao `ParkingEventRepository`

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingEventRepository.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingEventRepository.java
@@ -4,7 +4,9 @@ import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.ParkingEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParkingEventRepository extends JpaRepository<ParkingEvent, Long> {
-
+    Optional<ParkingEvent> findTopByLicensePlateAndExitTimeIsNullOrderByEntryTimeDesc(String licensePlate);
 }
