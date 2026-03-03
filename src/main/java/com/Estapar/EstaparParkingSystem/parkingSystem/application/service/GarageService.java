a0cea48f73612d91eac954304948fc850712d5ac
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-03 17:42:11 -0300

    ♻️ refactor: Remove método `importGarageConfig` e dependência de `SectorRepository` em `GarageService`

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/service/GarageService.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/service/GarageService.java
@@ -1,34 +1,15 @@
 package com.Estapar.EstaparParkingSystem.parkingSystem.application.service;
 
-import com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto.GarageConfigResponseDTO;
-import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.GarageSector;
-import jakarta.transaction.Transactional;
+import com.Estapar.EstaparParkingSystem.parkingSystem.domain.repository.GarageRepository;
+import com.Estapar.EstaparParkingSystem.parkingSystem.domain.repository.ParkingSpotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class GarageService{
+public class GarageService {
 
     private final GarageRepository garageRepository;
-    private final SectorRepository sectorRepository;
+    private final ParkingSpotRepository parkingSpotRepository;
 
-    @Transactional
-    @Override
-    public void importGarageConfig(GarageConfigResponseDTO dto) {
-
-        // evita duplicar se já existir
-        if (garageRepository.existsById(1L)) return;
-
-        GarageSector garage = new GarageSector();
-        garage.setId(1L);
-        garage.setCapacity(dto.getCapacity());
-        garageRepository.save(garage);
-
-        List<SectorEntity> sectors = dto.getSectors().stream()
-                .map(name -> new SectorEntity(null, name, garage))
-                .toList();
-
-        sectorRepository.saveAll(sectors);
-    }
 }
