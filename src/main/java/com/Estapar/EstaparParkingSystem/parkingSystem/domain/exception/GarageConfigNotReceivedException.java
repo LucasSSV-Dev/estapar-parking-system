4b59636846ad83f2438e9a6d0b910cb31b579797
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-04 18:07:49 -0300

    ✨ feat: Adiciona exceção personalizada `GarageConfigNotReceivedException`

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/exception/GarageConfigNotReceivedException.java
@@ -0,0 +1,7 @@
+package com.Estapar.EstaparParkingSystem.parkingSystem.domain.exception;
+
+public class GarageConfigNotReceivedException extends RuntimeException {
+    public GarageConfigNotReceivedException(String message) {
+        super(message);
+    }
+}
