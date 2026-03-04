04d72940f29e3e91cddfa1914d40eb20b614ba77
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-04 09:23:16 -0300

    ♻️ refactor: Renomeia `Sector` pra `sector` na `GarageConfigDTO` pra melhor consistência

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/dto/GarageConfigDTO.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/dto/GarageConfigDTO.java
@@ -3,7 +3,7 @@ package com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record GarageConfigDTO(
-        String Sector,
+        String sector,
         double basePrice,
 
         @JsonProperty("max_capacity")
