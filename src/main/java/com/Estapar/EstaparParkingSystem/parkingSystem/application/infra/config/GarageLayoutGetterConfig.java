65ecd684025cfb7088ac635ec25af71461ee935d
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-04 09:41:58 -0300

    ✨ feat: Adiciona `GarageLayoutGetterConfig` para importar layout da garagem ao iniciar aplicação

---
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/infra/config/GarageLayoutGetterConfig.java
@@ -0,0 +1,18 @@
+package com.Estapar.EstaparParkingSystem.parkingSystem.application.infra.config;
+
+import com.Estapar.EstaparParkingSystem.parkingSystem.application.service.GarageImportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GarageLayoutGetterConfig implements CommandLineRunner {
+
+    private final GarageImportService importService;
+
+    @Override
+    public void run(String... args) {
+        importService.importGarage();
+    }
+}
