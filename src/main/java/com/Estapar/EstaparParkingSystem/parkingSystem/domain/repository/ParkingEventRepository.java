c963643e40b83c4a05dbf6416e9c643b8fde6e3f
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-04 11:09:41 -0300

    ✨ feat: Adiciona query para calcular receita por data, setor e tipo de evento no `ParkingEventRepository`

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingEventRepository.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/domain/repository/ParkingEventRepository.java
@@ -1,12 +1,32 @@
 package com.Estapar.EstaparParkingSystem.parkingSystem.domain.repository;
 
+import com.Estapar.EstaparParkingSystem.parkingSystem.domain.enums.EventTypeEnum;
 import com.Estapar.EstaparParkingSystem.parkingSystem.domain.model.ParkingEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface ParkingEventRepository extends JpaRepository<ParkingEvent, Long> {
     Optional<ParkingEvent> findTopByLicensePlateAndExitTimeIsNullOrderByEntryTimeDesc(String licensePlate);
+
+    @Query("""
+    SELECT COALESCE(SUM(e.price), 0)
+    FROM ParkingEvent e
+    WHERE e.eventType = :eventType
+      AND e.sector = :sector
+      AND e.exitTime >= :startOfDay
+      AND e.exitTime < :endOfDay
+""")
+    Optional<BigDecimal> sumRevenueByDateAndSectorAndEventType(
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay,
+            @Param("sector") String sector,
+            @Param("eventType") EventTypeEnum eventType
+    );
 }
