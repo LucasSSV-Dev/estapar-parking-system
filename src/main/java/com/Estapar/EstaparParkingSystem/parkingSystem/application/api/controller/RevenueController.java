0694af61af0a5ec1393aad23d07b548ddbe4b728
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-03-04 14:58:53 -0300

    ✨ feat: Adiciona logging em `RevenueController`

--- src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/controller/RevenueController.java
+++ src/main/java/com/Estapar/EstaparParkingSystem/parkingSystem/application/api/controller/RevenueController.java
@@ -4,21 +4,27 @@ import com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto.Revenu
 import com.Estapar.EstaparParkingSystem.parkingSystem.application.api.dto.RevenueResponseDTO;
 import com.Estapar.EstaparParkingSystem.parkingSystem.application.service.RevenueService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/revenue")
 @RequiredArgsConstructor
+@Log4j2
 public class RevenueController {
 
     private final RevenueService revenueService;
 
     @GetMapping
+
     public ResponseEntity<RevenueResponseDTO> getRevenue(
             @RequestBody RevenueRequestDTO requestDTO
     ) {
+        log.info("[starts] RevenueController - getRevenue()");
         RevenueResponseDTO responseDTO = revenueService.calculateRevenue(requestDTO);
+        log.info("[ends] RevenueController - getRevenue()\n");
         return ResponseEntity.ok(responseDTO);
     }
 }
+
